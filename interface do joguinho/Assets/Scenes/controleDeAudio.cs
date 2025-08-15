3c91d17efe141789c13521bc55c372702a6f297e
Kimonaro <g.kira@escolar.ifrn.edu.br>
2025-08-14 21:08:45 -0300

    fim.

---
+++ interface do joguinho/Assets/Scenes/controleDeAudio.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+public class controleDeAudio : MonoBehaviour
+{
+    
+    public int Volume;
+    public int VolumeSFX;
+    public bool musica;
+
+    public Slider volumeSlider;
+    public Slider volumeSFXSlider;
+    public Toggle toggleMusica;
+    public TMP_Text textoMusica;
+
+    void Start()
+    {
+        musica = toggleMusica.isOn;
+        Volume = (int)volumeSlider.value;
+        VolumeSFX = (int)volumeSFXSlider.value;
+    }
+
+    void Update()
+    {
+        musica = toggleMusica.isOn;
+        Volume = (int)volumeSlider.value;
+        VolumeSFX = (int)volumeSFXSlider.value;
+        
+        if (musica == true)
+        {
+            textoMusica.text = "Ligado";
+            textoMusica.color = Color.green;
+        }
+        else
+        {
+            textoMusica.text = "Desligado";
+            textoMusica.color = Color.red;
+        }
+    }
+    
+}
